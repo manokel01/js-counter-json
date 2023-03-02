c5b7b1f453c68693aaa5bf4ca83dad5b348e789c
manofoo <manokel@gmail.com>
2023-03-02 23:53:31 +0200

    second commit

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
+
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 let jsonObj = document.getElementById("json-text");
-let count = 0;
-const d = new Date();
-let time = d.getTime;
-let json = {"count": count, "time": d};
+
+
+
+console.log(d);
 
 function increment() {
-    count += 1;
-    countEl.textContent = count;
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
-    
+    const d = new Date(); // gets new time for GMT
+    // let time = d.getTime(); for current time in milliseconds
     let countStr = count + " - ";
     saveEl.textContent += countStr;
+    let json = {"count": count, "time": d};
+    // jsonObj.textContent = count + d;
     jsonObj.textContent = JSON.stringify(json);
-    countEl.textContent = 0;
+    countEl.textContent = "0";
     count = 0;
-   
+}
+
+function send() {
+    let sendEl = document.getElementById("email")
+    window.open('mailto:manokel@gmail.com'  + sendEl.textContent)
+    window.open('mailto:manokel@gmail.com?subject=subject&body=body')
 }
 
 
